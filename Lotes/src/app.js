55683313bf840e5b28a783588ef4bef105e3fb59
Grupo-Acon <105016680+GrupoAcon@users.noreply.github.com>
2022-06-04 12:25:44 -0600

    Update app.js

--- Lotes/src/app.js
+++ Lotes/src/app.js
@@ -3,7 +3,7 @@ const root = ReactDOM.createRoot(
 
 	function tick(){
 		const date = (<p>{new Date().toLocaleDateString()}</p>)
-		const logo = "./img/"
+		const logo = "./img/logotrasp.png"
 		
 		const tropicales = `./pdfs/tropical.html`
 		const st_rosa = `./pdfs/st-rosa.html`
